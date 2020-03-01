04088d00f79f6c9e4c1b8c3b27941fc052d895be
jwadams75 <35203718+jwadams75@users.noreply.github.com>
2020-03-01 15:29:47 -0500

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,39 @@
+#Imports the zipped file
+install.packages("downloader")
+library(downloader)
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest = "power_data.zip")
+unzip("power_data.zip", exdir = "./")
+
+#Reads the zipped file
+install.packages("data.table")
+library(data.table)
+install.packages("readtext")
+library(readtext)
+power.data <- data.table(read.table("household_power_consumption.txt", header = T, sep = ";"))
+
+#Converts date variable to date format
+power.data$Date <- as.character(power.data$Date)
+power.data$Date <- as.Date(power.data$Date, format = "%d/%m/%Y")
+
+#Reduces the data set to the two dates needed for the project
+power.data <- power.data[Date >= "2007-02-01" & Date <= "2007-02-02",]
+
+#Plot 4
+power.data$Global_active_power <- as.numeric(as.character(power.data$Global_active_power))
+date.time <- as.POSIXct(paste(power.data$Date, power.data$Time))
+power.data$Voltage <- as.numeric(as.character(power.data$Voltage))
+power.data$Sub_metering_1 <- as.numeric(as.character(power.data$Sub_metering_1))
+power.data$Sub_metering_2 <- as.numeric(as.character(power.data$Sub_metering_2))
+power.data$Global_reactive_power <- as.numeric(as.character(power.data$Global_reactive_power))
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+plot(date.time, power.data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+plot(date.time, power.data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(date.time, power.data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(power.data$Sub_metering_2 ~ date.time, col = "red")
+lines(power.data$Sub_metering_3 ~ date.time, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, bty = "n", col = c("black", "red", "blue"))
+plot(date.time, power.data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
+
+
